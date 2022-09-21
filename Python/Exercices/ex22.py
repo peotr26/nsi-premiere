373e0ba540fababc8ddeb8517ddcb8f416fc8dcb
peotr26 <67782057+peotr26@users.noreply.github.com>
2022-09-21 18:34:49 +0200

    Travail à la maison du 21/09

--- Python/Exercices/ex22.py
+++ Python/Exercices/ex22.py
@@ -1,4 +1,13 @@
 def syra(a):
+    """Suite de Syracuse
+    
+    a : Nombre de départ
+    temps_vol : Le nombre de termes effectués
+    alt_max : Valeur maximal atteinte par a
+    nb_départ : Valeur du nombre de départ
+    temps_vol_alt : Nombre de termes consécutifs égal ou supérieur à a
+
+    """
     temps_vol = 0
     alt_max = a
     nb_start = a
