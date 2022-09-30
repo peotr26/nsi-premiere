0dc0921a175739807f0eea7b5a09dde4a0ca07a2
peotr26 <67782057+peotr26@users.noreply.github.com>
2022-09-30 20:51:20 +0200

    Travail maison 30/09

--- Python/Turtle/ex6.py
+++ Python/Turtle/ex6.py
@@ -1,20 +1,8 @@
 from turtle import *
 from random import randrange 
 
-def pantagone():
-    for i in range(0,10):
-        r1 = randrange(-400, 400)
-        r2 = randrange(-400, 400)
-        color()
-        up()
-        goto(r1, r2)
-        down()
-        for i in range(0, 6):
-            forward(30)
-            left(360/5)
-        up()
-        
-def color():
+
+def color_b(r1, r2):
     if r1 < 0 and r2 < 0:
         color('blue')
     if r1 < 0 and r2 > 0:
@@ -24,6 +12,19 @@ def color():
     if r1 > 0 and r2 >0:
         color('yellow')
 
+def pantagone():
+    for i in range(0,10):
+        r1 = randrange(-400, 400)
+        r2 = randrange(-400, 400)
+        color_b(r1, r2)
+        up()
+        goto(r1, r2)
+        down()
+        for i in range(0, 6):
+            forward(30)
+            left(360/5)
+        up()
+        
 pantagone()
 
 mainloop()
