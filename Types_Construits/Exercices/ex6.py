bb6fc7bc88cee4bbc51d487f1afb8014ffed78bf
peotr26 <67782057+peotr26@users.noreply.github.com>
2022-11-19 11:29:21 +0100

    Travail maison 19/11

--- Types_Construits/Exercices/ex6.py
+++ Types_Construits/Exercices/ex6.py
@@ -1,10 +1,10 @@
 def double_voyelle(ch):
-    voyelle = ['a','e','u','o','i','y']
+    voyelles = ['a','e','u','o','i','y']
     n = len(ch)
     final = ''
     for i in range(0,n):
         final += ch[i]
-        if ch[i] == voyelle[]:
+        if any(voyelle in ch[i] for voyelle in voyelles):   # Vérifie si le charactère correspond à un charactère dans la liste voyelles.
             final += ch[i]
     return final
 
---
+++ Types_Construits/Exercices/ex7.py
@@ -0,0 +1,10 @@
+def separation(ch):
+    n = len(ch)
+    final = ''
+    for i in range(0,n-1):
+        final += ch[i]
+        final += '*'
+    final += ch[n-1]
+    return final
+
+print(separation('hello'))
