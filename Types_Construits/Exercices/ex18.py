d0de5f90750d008c82bcecda53d98a8d5e728a77
peotr26 <67782057+peotr26@users.noreply.github.com>
2022-12-04 11:12:06 +0100

    Travail maison 4/12

---
+++ Types_Construits/Exercices/ex18.py
@@ -0,0 +1,7 @@
+def xor(x:bool,y:bool)->bool:
+    return x != y
+
+print(xor(True, False))
+print(xor(False, True))
+print(xor(False, False))
+print(xor(True, True))
--- ex18.py
+++
@@ -1,5 +0,0 @@
-def xor(x:bool,y:bool)->bool:
-    result = (x and not y) or (not x and y)
-    return result
-
-print(xor(True,False))
