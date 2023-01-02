95e6f2d83ff3bd7cd6de69a2bf1674876d01f17b
peotr26 <67782057+peotr26@users.noreply.github.com>
2023-01-02 18:36:42 +0100

    Travail maison 2/1
    L'exercice 8 est loin d'être terminé.

--- Encodage/Exercices/ex8.py
+++ Encodage/Exercices/ex8.py
@@ -1,16 +1,47 @@
+def invert(binary:list)->list:
+    n = len(binary)
+    for i in range(0, n):
+        temp = binary[i]
+        if temp == 0:
+            binary[i] = 1
+        else:
+            binary[i] = 0
+    return binary
+
+assert invert([1,0,0,0]) == [0,1,1,1]
+
+def add1(binary:list, n:int, ret:int)->list:
+    for i in range(0, n-1):
+        if binary[i] == 0 and ret == 0:
+            binary[i] = 0
+            ret = 0
+        elif binary[i] == 0 and ret == 1:
+            binary[i] = 1
+            ret = 0
+        elif binary[i] == 1 and ret == 0:
+            binary[i] = 1
+            ret = 0
+        else:
+            binary[i] = 1
+            ret = 1
+    return binary
+
+def addition(binary:list)->list:
+    n = len(binary)
+    ret = 0
+    if binary[n-1] == 0:
+        binary[n-1] = 1
+    else:
+        binary[n-1] = 0
+        ret = 1
+        add1(binary, n, ret)
+    return binary
+
+assert addition([0,1,1,1]) == [1,1,1,0]
+
 def complement_deux(b):
-    n = len(b)
-    result = 0
-    a = 0
-    for i in range(n-1, -1, -1):
-        var = b.pop(i)
-        if var == 1:
-            result += 2**a
-        a += 1
+    result = invert(b)
+    result = addition(result)
     return result
 
-b1 = [0,1,1,1]
-assert complement_deux(b1) == 7
-
-b2 = [1,1,1,0,0,1,0]
-assert complement_deux(b2) == 114
+print(complement_deux([1,1,1,0]))
