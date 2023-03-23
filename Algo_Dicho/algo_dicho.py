410350dda2fe9af5e4e439d559a5d6923461e612
peotr26 <67782057+peotr26@users.noreply.github.com>
2023-03-23 16:07:16 +0100

    Cours du 23/03

--- Algo_Dicho/algo_dicho.py
+++ Algo_Dicho/algo_dicho.py
@@ -1,13 +1,13 @@
-def recherche_di(L, e):
+def recherche(L: list, e: int) -> int:
     debut = 0
-    fin = len(L)
+    fin = len(L)-1
     milieu = (debut+fin)//2
     pre = fin-debut
     while L[milieu] != e:
         if L[milieu] < e:
-            debut = milieu
+            debut = milieu+1
         elif L[milieu] > e:
-            fin = milieu
+            fin = milieu-1
         milieu = (debut+fin)//2
         if pre == fin-debut:
             return -1
@@ -17,8 +17,8 @@ def recherche_di(L, e):
 
 L = [0, 2, 4, 7, 8, 13]
 resultat = []
-for i in range(-2, 15):
-    resultat.append(recherche_di(L, i))
+for e in range(-2, 15):
+    resultat.append(recherche(L, e))
 assert resultat == [
     -1, -1, 0, -1, 1, -1, 2, -1, -1, 3, 4, -1, -1, -1, -1, 5, -1
 ]
